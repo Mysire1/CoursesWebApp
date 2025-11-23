135e3684902780838cc81d61b3d0a8b14c4e6dd2
Kostiantyn Smozhevskykh <159162697+Mysire1@users.noreply.github.com>
2025-11-23 12:37:17 +0200

    Інтерфейс ILanguageService: додано метод GetAllLanguagesWithGroupCountAsync для отримання кількості груп за мовами, та DTO LanguageWithGroupCount.

--- Services/ILanguageService.cs
+++ Services/ILanguageService.cs
@@ -1,14 +1,23 @@
 using CoursesWebApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CoursesWebApp.Services
 {
     public interface ILanguageService
     {
         Task<IEnumerable<Language>> GetAllLanguagesAsync();
+        Task<IEnumerable<LanguageWithGroupCount>> GetAllLanguagesWithGroupCountAsync();
         Task<Language?> GetLanguageByIdAsync(int id);
         Task<decimal> CalculateLanguageCostAsync(int languageId);
         Task<Dictionary<string, decimal>> CalculateAllLanguagesCostAsync();
         Task<Dictionary<string, decimal>> CalculateCostByLevelAsync(int languageId);
         Task<decimal> CalculateMonthlyLanguageCostAsync(int languageId);
+    }
+
+    public class LanguageWithGroupCount
+    {
+        public Language Language { get; set; } = null!;
+        public int GroupCount { get; set; }
     }
 }
